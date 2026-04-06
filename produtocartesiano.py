bf7cf4d543d4a64b46c72b515334ec1e7d5649f5
amad-nicolas <103444403+amad-nicolas@users.noreply.github.com>
2026-04-05 21:10:52 -0300

    visualização do produto cartesiano usando matplotlib
    Added scatter plot visualization for Cartesian product of A and B.

--- produtocartesiano.py
+++ produtocartesiano.py
@@ -1,3 +1,5 @@
+import matplotlib.pyplot as plt
+
 A=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
 B=[4,5,6,7,8,9,10,11,27,28,30,31,33,36,39,45]
 
@@ -6,6 +8,8 @@ AXB=[]
 for i in range(len(A)):
                for j in range(len(B)):
                    AXB.append((A[i],B[j]))
+                   plt.scatter(A[i],B[j])
 
+plt.show()
 print(AXB)
                    
